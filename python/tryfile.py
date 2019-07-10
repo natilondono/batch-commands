727515e77a2631e81daf872d67e05800c0a6dd3d
natilondono <36642605+natilondono@users.noreply.github.com>
2019-07-10 11:10:40 +0200

    Update tryfile.py

--- python/tryfile.py
+++ python/tryfile.py
@@ -1,1 +1,9 @@
-print("blah")
+import numpy as np
+import pandas as pd
+print ("blah")
+import matplotlib.pyplot as plt
+from ripser import ripser
+from persim import plot_diagrams
+data = pd.read_csv("graphs/facebook_mess_norm.txt", sep=" ")
+dgms = ripser(data)['dgms']
+plot_diagrams(dgms, show=True)
